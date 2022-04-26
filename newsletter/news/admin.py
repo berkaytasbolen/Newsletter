c02efacd3ed4a29fade92e7f249bf13df8ceb3f2
Ali Berkay Taşbölen <96075404+berkaytasbolen@users.noreply.github.com>
2022-04-26 18:29:23 +0300

    first commit
    I created the classes and serialiers

---
+++ newsletter/news/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from news.models import Makale
+# Register your models here.
+admin.site.register(Makale)
---
+++ newsletter/news/models.py
@@ -0,0 +1,18 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+# Create your models here.
+
+class Makale(models.Model):
+    yazar = models.CharField(max_length=150)
+    baslik = models.CharField(max_length=120)
+    aciklama = models.CharField(max_length=200)
+    metin = models.TextField()
+    sehir = models.CharField(max_length=120)
+    yayimlanma_tarihi = models.DateField()
+    aktif = models.BooleanField(default=True)
+    yaratilma_tarihi = models.DateTimeField(auto_now_add=True)
+    güncelleme_tarihi = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.baslik
